88e4b08683ed475ecf6f41eda625de1a5ccadf62
trzl <brownty47@gmail.com>
2018-07-21 17:27:09 +0000

    fixing build file issues

--- build.sh
+++ build.sh
@@ -2,12 +2,9 @@
 docker-compose -f build.yml up --build -d && \
 docker system prune --volumes && \
 mkdir services/react-nginx/build && \
-cp -r services/react/build/server services/react-nginx/build && \
 cp -r services/react/build/client services/react-nginx/build && \
-docker build -t trzl/portfolio:augment-react.1 -f ./services/react-nginx/Dockerfile.prod ./services/react-nginx && \
-# docker push trzl/portfolio:augment-react.1 && \
-# docker build -t trzl/portfolio:augment-apollo.2 -f ./services/apollo/Dockerfile ./services/apollo && \
-# docker push trzl/portfolio:augment-apollo.2 && \
+docker build -t trzl/portfolio:bonsai-react.1 -f ./services/react-nginx/build.Dockerfile ./services/react-nginx && \
+# docker push trzl/portfolio:bonsai-react.1 && \
 docker-compose down && \
 docker system prune --volumes && \
 sudo rm -rf services/react-nginx/build && \
